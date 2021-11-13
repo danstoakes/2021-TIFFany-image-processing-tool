4b006a31b79fc1af52f846557dd9282e6721228d
Dan Stoakes <32640999+danstoakes@users.noreply.github.com>
2021-11-13 21:41:49 +0000

    Update __init__.py

--- TIFFany/__init__.py
+++ TIFFany/__init__.py
@@ -1,3 +1,4 @@
+# import the modules which will be accessible via the package
 from .process import mse, ssim
 from .utility import convert_to_gray, convert_to_grey, convert_to_RGB
 from .visualise import contours, foreground_mask, gaussian_blur, median_blur, show
